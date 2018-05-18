9edf5df9a120ea3211c0c1c3ada88f7aa6cf7bb2
Gonza <gonzaloalcalde96@gmail.com>
2018-05-17 23:40:02 -0300

    Fixes

--- app/src/main/java/ar/edu/undav/queestasleyendo/LoginActivity.java
+++ app/src/main/java/ar/edu/undav/queestasleyendo/LoginActivity.java
@@ -159,13 +159,17 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         boolean cancel = false;
         View focusView = null;
 
-        // Check for a valid password, if the user entered one.
+        // Check for a valid password.
         if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
         }
-
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_password_required));
+            focusView = mPasswordView;
+            cancel = true;
+        }
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
             mEmailView.setError(getString(R.string.error_field_required));
